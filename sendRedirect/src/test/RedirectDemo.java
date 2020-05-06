1fc6116b3ec161821d2f20310928aa73075c6ad1
Avanti Joshi <59311642+Avanti26@users.noreply.github.com>
2020-05-06 17:43:01 +0530

    FOOGLE
    redirects to google search engine

---
+++ sendRedirect/src/test/RedirectDemo.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/searchlink")
+public class RedirectDemo extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		String searchfactor=req.getParameter("search");
+		
+		resp.sendRedirect("https://www.google.com/#q="+searchfactor);
+	}
+}
